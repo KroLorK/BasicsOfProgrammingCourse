1b09d803636f0c3396173f9e07b8d1ee40f27a6b
KroLorK <kotelnikov818@gmail.com>
2023-04-09 23:51:28 +0300

    append access functions

--- main.c
+++ main.c
@@ -12,10 +12,32 @@ void test_popBack_notEmptyVector() {
     assert(v.capacity == 1);
 }
 
+void test_atVector_notEmptyVector() {
+    Vector v = createVector(2);
+    pushBack(&v, 42);
+    assert(atVector(&v, 0) == &v.data[0]);
+}
+
+void test_back_oneElementInVector() {
+    Vector v = createVector(2);
+    pushBack(&v, 24);
+    assert(back(&v) == &v.data[0]);
+}
+
+void test_front_oneElementInVector() {
+    Vector v = createVector(2);
+    pushBack(&v, 84);
+    assert(front(&v) == &v.data[0]);
+}
+
 int main() {
     //Vector v = createVector(10);
 
     test_popBack_notEmptyVector();
+    test_atVector_notEmptyVector();
+    test_back_oneElementInVector();
+    test_front_oneElementInVector();
+
     //reserve(&v, SIZE_MAX);
 
     return 0;
